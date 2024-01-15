cfd5e590bdd5c97ce101e5ef4eeddc64c055f58e
sutanu-bera <berasutanu02@gmail.com>
2024-01-15 20:30:49 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+  
+public static String revString(String str) {
+   if(str.length() == 1) {
+   return str;
+  }
+  char currChar = str.charAt(0);
+  String nextString = revString(str.substring(1));
+  return nextString + currChar;
+ }
+  
+public static void main(String args[]) {
+
+   String str = "abcd";
+   String reversed = revString(str);
+   System.out.println(reversed);
+  }
+}
